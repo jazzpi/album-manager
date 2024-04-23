b252197b826f78b64ad1b8e55e2dd37a6ca2c221
jazzpi <jasper@mezzo.de>
2024-04-24 00:52:56 +0200

    Show multiple albums in a grid

---
+++ src/lib/components/album-display.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Album } from '@spotify/web-api-ts-sdk';
+
+	export let album: Album;
+
+	$: artists = album.artists.map((artist) => artist.name).join(', ');
+	$: image_sources = album.images.map((image) => `${image.url} ${image.width}w`).join(', ');
+	$: image_sizes = album.images.map((image) => `${image.width}px`).join(', ');
+</script>
+
+<img srcset={image_sources} sizes={image_sizes} alt="Album cover" />
+<div class="name">{album.name}</div>
+<div class="artists">{artists}</div>
+
+<style>
+	img {
+		width: 100%;
+		height: auto;
+	}
+	.artists {
+		text-align: center;
+	}
+	.name {
+		text-align: center;
+		font-weight: bold;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,36 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
-    import { initSDK } from '../spotify';
-    import { PUBLIC_CLIENT_ID } from '$env/static/public';
-    const sdk = initSDK(PUBLIC_CLIENT_ID);
-    let artist = '';
-    let title = '';
-    onMount(async () => {
-        // TODO
-        const id = '5KFtfsMy5lxO5UKVq83O2N';
-        let album = await sdk.albums.get(id);
-        artist = album.artists[0].name;
-        title = album.name;
-    });
+	import { initSDK } from '$lib/spotify';
+	import { PUBLIC_CLIENT_ID } from '$env/static/public';
+	import AlbumDisplay from '$lib/components/album-display.svelte';
+	import type { Album } from '@spotify/web-api-ts-sdk';
+
+	const sdk = initSDK(PUBLIC_CLIENT_ID);
+	let albums: Album[] = [];
+	// TODO
+	let album_ids = [
+		'5KFtfsMy5lxO5UKVq83O2N',
+		'66h0WavoqRQwNu18mzPIZH',
+		'2WBrAkDpKDtCn7Ueyeg6m8',
+		'4YLMKLoi42lAGbji4ZlMXH'
+	];
+	onMount(async () => {
+		albums = await sdk.albums.get(album_ids);
+	});
 </script>
 
-<p>{artist} - {title}</p>
+<div id="albums" class="pure-g">
+	{#each albums as album}
+		<div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+			<div>
+				<AlbumDisplay {album} />
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	#albums.pure-g > div > div {
+		padding: 0.5em;
+	}
+</style>
