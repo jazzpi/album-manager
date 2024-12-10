3e4dacc0e610cff893d09ad95dc7a4d5bbb86d9a
jazzpi <jasper@mezzo.de>
2024-12-10 12:21:14 +0100

    Work around broken Spotify Web API

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -2,7 +2,8 @@ import {
 	SpotifyApi,
 	type AccessToken,
 	type IAuthStrategy,
-	type Image
+	type Image,
+	type IResponseDeserializer
 } from '@spotify/web-api-ts-sdk';
 import { page } from '$app/stores';
 import { get } from 'svelte/store';
@@ -38,7 +39,27 @@ class NextAuthStrategy implements IAuthStrategy {
 	}
 }
 
-export const sdk = new SpotifyApi(new NextAuthStrategy());
+// The Spotify Web API has started returning some kind of ID where it should
+// return empty responses according to the docs. This leads to a JSON parse
+// error in the SDK. We work around this by returning null if the Content-type
+// is not JSON. Taken from
+// https://github.com/spotify/spotify-web-api-ts-sdk/issues/127#issuecomment-2439575131
+class BrokenResponseDeserializer implements IResponseDeserializer {
+	async deserialize<TReturnType>(response: Response): Promise<TReturnType> {
+		const text = await response.text();
+		const contentType = response.headers.get('content-type') ?? '';
+		if (text.length > 0 && contentType.includes('application/json')) {
+			const json = JSON.parse(text);
+			return json as TReturnType;
+		}
+
+		return null as unknown as TReturnType;
+	}
+}
+
+export const sdk = new SpotifyApi(new NextAuthStrategy(), {
+	deserializer: new BrokenResponseDeserializer()
+});
 
 export function getMaxResolutionImage(images: Image[]): Image {
 	return images.reduce((prev, current) => {
